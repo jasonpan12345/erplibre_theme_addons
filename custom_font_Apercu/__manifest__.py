fdc63e3dc85cf0a2af43903914e4d1d13a589f10
Carms Ng <carmenhyng89@gmail.com>
2021-02-04 12:45:47 -0500

    [Add] module to add custom font Apercu

---
+++ custom_font_Apercu/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Custom Font Apercu',
+    'version': '0.1',
+    'author': 'TechnoLibre',
+    'website': 'https://technolibre.ca',
+    'license': 'AGPL-3',
+    'category': 'Data',
+    'summary': 'Add custom font - Apercu',
+    'description': """
+Custom Font Apercu
+==================
+Add styling asset.
+""",
+    'depends': [
+        "web",
+    ],
+    'data': [
+        'data/assets.xml',
+    ],
+    "auto_install": False,
+    "installable": True,
+    "application": False,
+}
