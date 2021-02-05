14ef76385ebba7b7548d4f4af86c9979e6de9137
Carms Ng <carmenhyng89@gmail.com>
2021-02-05 18:30:42 -0500

    [WIP] Added btn outline snippet

---
+++ erplibre_website_snippets/__manifest__.py
@@ -0,0 +1,18 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'ERPLibre Website Snippets',
+    'summary': 'Additional Website Snippets',
+    'version': '12.0.1.1.0',
+    'category': 'Website',
+    'license': 'AGPL-3',
+    'author': 'TechnoLibre',
+    'website': 'https://technolibre.ca',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'website',
+    ],
+    'data': [
+        'views/snippets.xml',
+    ],
+}
