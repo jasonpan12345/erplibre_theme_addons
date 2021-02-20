06f5451c44fd5c2a72408de1b7860b399898dbd4
Carms Ng <carms.ng@technolibre.ca>
2021-02-19 22:44:06 -0500

    [ADD] erplibre website snippets: animated card structures - flippable and extendable cards

---
+++ erplibre_website_snippets_cards/__manifest__.py
@@ -0,0 +1,15 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+  'name': 'ERPLibre Website Snippets: Cards',
+  'category': 'Website',
+  'summary': 'Card website snippets that are flippable and extendable on click',
+  'version': '12.0.1.0.0',
+  'author': 'TechnoLibre',
+  'website': 'https://technolibre.ca',
+  'license': 'AGPL-3',
+  'application': True,
+  'depends': ['website'],
+  'data': ['views/snippets.xml'],
+  'installable': True,
+}
