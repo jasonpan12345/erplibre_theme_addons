d20a83683e1c0947f661cf771b18434f7fa854b8
Carms Ng <carms.ng@technolibre.ca>
2021-02-19 22:37:55 -0500

    [ADD] erplibre website snippets: sectional structures - 3-step how it works, linked image grid, 5-part text panel

---
+++ erplibre_website_snippets_structures/__manifest__.py
@@ -0,0 +1,15 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+  'name': 'ERPLibre Website Snippets: Structures',
+  'category': 'Website',
+  'summary': '3-step (how it works), linked image grid, and 5-part text panel',
+  'version': '12.0.1.0.0',
+  'author': 'TechnoLibre',
+  'website': 'https://technolibre.ca',
+  'license': 'AGPL-3',
+  'application': True,
+  'depends': ['website'],
+  'data': ['views/snippets.xml'],
+  'installable': True,
+}
