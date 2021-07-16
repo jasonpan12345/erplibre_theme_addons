551d160d8c212e9faafc41d7aa0ea1fe9c54f092
Mathieu Benoit <mathben@technolibre.ca>
2021-07-16 19:26:15 -0400

    [FIX] snippets are not application

--- erplibre_website_snippets_timelines/__manifest__.py
+++ erplibre_website_snippets_timelines/__manifest__.py
@@ -8,7 +8,7 @@
   'author': 'TechnoLibre',
   'website': 'https://technolibre.ca',
   'license': 'AGPL-3',
-  'application': True,
+  'application': False,
   'depends': ['website'],
   'data': ['views/snippets.xml'],
   'installable': True,
