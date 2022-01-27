0ee4d9d5601dc599807b7b4201ac98b4f9c8419c
Jason Pan <jason.pan@technolibre.ca>
2022-01-27 18:35:57 -0500

    [ADD] erplibre_website_snippets_helloworld: WIP snippet helloworld

---
+++ erplibre_website_snippets_helloworld/__manifest__.py
@@ -0,0 +1,15 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+  'name': 'ERPLibre Website Snippets: Hello World',
+  'category': 'Website',
+  'summary': 'Hello World snippet test',
+  'version': '12.0.1.0.0',
+  'author': 'TechnoLibre',
+  'website': 'https://technolibre.ca',
+  'license': 'AGPL-3',
+  'application': False,
+  'depends': ['website'],
+  'data': ['views/snippets.xml'],
+  'installable': True,
+}
